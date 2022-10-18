2dbc1f982b38b9c08e5436a982fa245e26d74ddb
Shelby Ziccardi <shelbyziccardi@gmail.com>
2022-10-17 19:47:49 -0500

    finish cleaning out MM

--- movement_metrics.py
+++ movement_metrics.py
@@ -1,8 +1,5 @@
-import os
-from statistics import mean, median
-import sys
+from statistics import mean
 import json
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 import scipy.signal as signal
@@ -11,38 +8,12 @@ import math
 
 class PlotType(Enum):
     NONE = 0
-    POINT_CLOUD = 1
-    CENTROID = 2
-    POS_SPEC = 3
-    CENT_DIST = 4
-    CENT_DIST_NORM = 5
 
-    VEL_HEAT_MAP = 6
-    TOTAL_VEL_OVER_TIME = 7
-    VEL_OVER_TIME = 8
+    REL_POS = 1
+    REL_POS_OVER_TIME = 2
 
-    APERATURE = 9
-
-    ACCEL_TREE = 10
-
-    REL_POS = 11
-    REL_POS_OVER_TIME = 12
-    MOV_HEAT_MAP = 13
-    ANGLE_OVER_TIME = 14
-    ANGLE_HIST = 15
-    
-    REL_ANGLES = 16
-
-plot_type_dict = dict({"point cloud":PlotType.POINT_CLOUD, "centroid of motion":PlotType.CENTROID, 
-    "position spectrum":PlotType.POS_SPEC, "distance from center":PlotType.CENT_DIST, 
-    "normalized distance from center":PlotType.CENT_DIST_NORM, "velocity heat map":PlotType.VEL_HEAT_MAP, 
-    "speed over time":PlotType.TOTAL_VEL_OVER_TIME, "velocity over time":PlotType.VEL_OVER_TIME, 
-    "aperature over time":PlotType.APERATURE, "accelerometer tree":PlotType.ACCEL_TREE,
-    "relative position":PlotType.REL_POS, "relative position over time":PlotType.REL_POS_OVER_TIME, 
-    "movement heatmap":PlotType.MOV_HEAT_MAP, "angles over time":PlotType.ANGLE_OVER_TIME, 
-    "angle histogram":PlotType.ANGLE_HIST, "relative angles":PlotType.REL_ANGLES})
+plot_type_dict = dict({"relative position":PlotType.REL_POS, "relative position over time":PlotType.REL_POS_OVER_TIME})
 keypoint_names = ['head','spine_top','shoulder_right','elbow_right','wrist_right','shoulder_left', 'elbow_left','wrist_left']
-#'spine_base','hip_right','knee_right','ankle_right','hip_left', 'knee_left','ankle_left','eye_right','eye_left','ear_right','ear_left','big_toe_left', 'little_toe_left','heel_left','big_toe_right','little_toe_right','heel_right','palm_left', 'thumb_base_left','thumb_1_left','thumb_2_left','thumb_tip_left','pointer_base_left', 'pointer_1_left','pointer_2_left','pointer_tip_left','middle_base_left','middle_1_left', 'middle_2_left','middle_tip_left','ring_base_left','ring_1_left','ring_2_left','ring_tip_left', 'pinky_base_left','pinky_1_left','pinky_2_left','pinky_tip_left','palm_right','thumb_base_right', 'thumb_1_right','thumb_2_right','thumb_tip_right','pointer_base_right','pointer_1_right', 'pointer_2_right','pointer_tip_right','middle_base_right','middle_1_right','middle_2_right', 'middle_tip_right','ring_base_right','ring_1_right','ring_2_right','ring_tip_right', 'pinky_base_right','pinky_1_right','pinky_2_right','pinky_tip_right'
 keypoint_nums = list(np.arange(1,len(keypoint_names)+1))
 stoi_map = dict(zip(keypoint_names, keypoint_nums))
 itos_map = dict(zip(keypoint_nums, keypoint_names))
@@ -120,50 +91,6 @@ def ReadDataFromList(files, img_size):
 
     return vals
 
-def ReadDataInFolder(file_path):
-    vals =[]
-    
-    for filename in os.listdir(file_path+'/pose_info'):
-        
-        if ".json" in filename:
-            with open(os.path.join(file_path, filename), 'r') as f: 
-                #print("read " + filename)
-                lines = f.readlines()
-                json_file = json.loads(lines[0])
-
-                data_array = json_file['people'][0]['pose_keypoints_2d']
-                x_pose = data_array[::3]
-                y_pose = [700 - x for x in data_array[1::3]]
-                c_pose = data_array[2::3]
-
-                #flipped left and right to respect participant left/right vs frame left/right
-                data_array = json_file['people'][0]['hand_left_keypoints_2d'] 
-                x_right_hand = data_array[::3]
-                y_right_hand = [700 - x for x in data_array[1::3]]
-                c_right_hand = data_array[2::3]
-
-                data_array = json_file['people'][0]['hand_right_keypoints_2d']
-                x_left_hand = data_array[::3]
-                y_left_hand = [700 - x for x in data_array[1::3]]
-                c_left_hand = data_array[2::3]
-
-                x = []
-                x.extend(x_pose)
-                x.extend(x_right_hand)
-                x.extend(x_left_hand)
-                y = []
-                y.extend(y_pose)
-                y.extend(y_right_hand)
-                y.extend(y_left_hand)
-                c = []
-                c.extend(c_pose)
-                c.extend(c_right_hand)
-                c.extend(c_left_hand)
-
-                vals.append([x,y,c])
-    
-
-    return vals
 
 #Metric helpers
 def getMin(data):
@@ -175,12 +102,6 @@ def getMax(data):
 def getMean(data):
     return mean(data)
 
-def getMedian(data):
-    return median(data)
-
-def getQuartile(data, q):
-    return np.percentile(data, q*25)
-
 def getVariance(data):
     return np.var(data)
 
@@ -190,29 +111,9 @@ def getCoVariance(data1, data2):
 def getSTD(data):
     return np.std(data)
 
-def getSpread(data1, data2):
-    c = np.cov(np.vstack((data1, data2)))
-    w = np.linalg.eigvals(c)
-    s = 5.991
-    major_ax = np.sqrt(s * w[0])
-    minor_ax = np.sqrt(s * w[1])
-
-    return np.pi * major_ax * minor_ax
-
-def getCorrelationR(data1, data2):
-    return stats.pearsonr(data1, data2)
 
 def getPeaks(data, my_threshold):
     return signal.find_peaks(data, threshold=my_threshold)
-
-def getCorrelationCross(data1, data2):
-    ccov = np.correlate(data1 - data1.mean(), data2 - data2.mean())
-    ccov_test = np.correlate(data1 - data1.mean(), data2 - data2.mean(), mode='full')
-    ccor = ccov / (len(data1) * data1.std() * data2.std())
-    ccor_test = ccov_test / (len(data1) * data1.std() * data2.std())
-    print(ccor)
-    print(max(ccor_test))
-    return ccor
 
 
 def getAxesCrossedCounts(data, start_less):
